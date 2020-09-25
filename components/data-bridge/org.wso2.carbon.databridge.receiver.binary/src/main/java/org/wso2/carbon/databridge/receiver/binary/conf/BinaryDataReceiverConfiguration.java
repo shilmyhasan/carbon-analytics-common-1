9d43cbae6914f8a395bb7e86aac2b14b7f488552
Wasura Wattearachchi <wasuradananjith@gmail.com>
2020-09-25 12:42:47 +0530

    Revert "[5.2.24] Alternative fix for https://github.com/wso2/product-apim/issues/8144"

--- components/data-bridge/org.wso2.carbon.databridge.receiver.binary/src/main/java/org/wso2/carbon/databridge/receiver/binary/conf/BinaryDataReceiverConfiguration.java
+++ components/data-bridge/org.wso2.carbon.databridge.receiver.binary/src/main/java/org/wso2/carbon/databridge/receiver/binary/conf/BinaryDataReceiverConfiguration.java
@@ -32,6 +32,7 @@ public class BinaryDataReceiverConfiguration {
     private int tcpPort;
     private int sizeOfSSLThreadPool;
     private int sizeOfTCPThreadPool;
+    private int waitingTimeInMilliSeconds;
     private String sslProtocols;
     private String ciphers;
 
@@ -55,6 +56,8 @@ public class BinaryDataReceiverConfiguration {
         this.sizeOfTCPThreadPool = Integer.parseInt(dataReceiver.getConfiguration(
                 BinaryDataReceiverConstants.TCP_RECEIVER_THREAD_POOL_SIZE,
                 BinaryDataReceiverConstants.DEFAULT_TCP_RECEIVER_THREAD_POOL_SIZE).toString());
+        this.waitingTimeInMilliSeconds = Integer.parseInt(dataReceiver.getConfiguration(
+                BinaryDataReceiverConstants.WAITING_TIME_IN_MILISECONDS, 0).toString());
 
         this.enable = Boolean.valueOf(dataReceiver.getConfiguration(BinaryDataReceiverConstants.ENABLE_BINARY_DATA_RECEIVER, enable).toString());
         Object sslProtocolObj = dataReceiver.getConfiguration(BinaryDataReceiverConstants.SSL_RECEIVER_PROTOCOLS_CONFIG_NAME, null);
@@ -89,6 +92,10 @@ public class BinaryDataReceiverConfiguration {
 
     public String getCiphers() {
         return ciphers;
+    }
+
+    public int getWaitingTimeInMilliSeconds() {
+        return waitingTimeInMilliSeconds;
     }
 
     public boolean isEnable() {
